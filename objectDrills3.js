d750e6b8830e24bec040fa1b5c95b496d84da3a2
Jordan Heffernan <jheff2@gmail.com>
2018-12-05 14:05:13 -0800

    added characters array

--- objectDrills3.js
+++ objectDrills3.js
@@ -130,7 +130,8 @@ function decodeWords (string) {
 
 decodeWords("craft block argon meter bells brown croon droop");
 
-const test = createCharacter('evan', 'billy', 'white', 'heaven', 0, 0 )
+const test1 = new createCharacter('evan', 'billy', 'white', 'heaven', 10, 8 )
+const test2 = new createCharacter('Jordan', 'Big Cat', 'white', 'heaven', 7, 20)
 
 function createCharacter (name, nickname, race, origin, attack, defense ) {
   return {
@@ -140,13 +141,25 @@ function createCharacter (name, nickname, race, origin, attack, defense ) {
     origin,
     attack,
     defense,
-    describe: function() {return `${name} is a ${race} from ${origin}.`}
+	describe: function() {return `${name} is a ${race} from ${origin}.`},
+	evaluateFight: function(character) {
+		let damage = character.attack - this.defense;
+		if (damage > 0) { 
+		return `Your opponent takes ${attack} damage and you recieve ${damage} damage.`
+	} else {return `Your opponent takes ${attack} damage and you recieve 0 damage.`}
   }
 }
+ }
 
-
-
-
-console.log(test.describe());
-
-
+ const characters = [
+	createCharacter("Gandalf the White", "gandalf", "Wizard", "Middle Earth", 10, 6,), 
+	createCharacter("Bilbo Baggins", "bilbo", "Hobbit", "The Shire", 2, 1), 
+	createCharacter("Frodo Baggins", "frodo", "Hobbit", "The Shire", 3, 2),
+	createCharacter("Aragorn son of Arathorn", "aragorn", "Man", "Dunnedain", 6, 8),
+	createCharacter("Legolas", "legolas", "Elf", "Woodland Realm", 8, 5),
+	createCharacter("Arwen Undomiel", "Arwen", "Half-Elf", "Rivendell", 40, 80)
+	
+];
+console.log(characters);
+// console.log(test1.describe());
+// console.log(test2.evaluateFight(test1))
