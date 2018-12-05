020fae89c4c4551553d0267b9d1ea54d31b96839
Jordan Heffernan <jheff2@gmail.com>
2018-12-05 11:44:40 -0800

    just checking

--- objectDrills2.js
+++ objectDrills2.js
@@ -1,21 +1,21 @@
 function makeStudentsReport(data) {
-    const answer = data.map(student => `${student.name}: ${student.grade}`)
-    return answer
-  }
+  const answer = data.map(student => `${student.name}: ${student.grade}`)
+  return answer
+}
 
-  const students = [
-    {
-      name: 'Tim',
-      status: 'Current student',
-      course: 'Biology'
-    },
-    {
-      name: 'Sue',
-      status: 'Withdrawn',
-      course: 'Mathematics'
-    }
-  ];
-  
+const students = [
+  {
+    name: 'Tim',
+    status: 'Current student',
+    course: 'Biology'
+  },
+  {
+    name: 'Sue',
+    status: 'Withdrawn',
+    course: 'Mathematics'
+  }
+];
+
 
 // function enrollInSummerSchool (students) {
 //   const summerSchool = students.map(student => {
@@ -24,7 +24,7 @@ function makeStudentsReport(data) {
 //       status: 'In Summer School',
 //       course: student.course
 //     }
-    
+
 //   });
 //   return summerSchool;
 // }
@@ -33,134 +33,13 @@ function makeStudentsReport(data) {
 // console.log(enrollInSummerSchool(students));
 
 
-function findByID (items, idNum) {
-    const found = items.find(item => {
-        return item.id === idNum
-    } );
-    return found;
+function findByID(items, idNum) {
+  const found = items.find(item => {
+    return item.id === idNum
+  });
+  return found;
 }
 
-const dataID = [{id: 1, foo: 'bar'}, {id: 2, foo: 'bizz'}];
+const dataID = [{ id: 1, foo: 'bar' }, { id: 2, foo: 'bizz' }];
 
-// console.log(findByID(dataID, 1))
-
-// running the function with `objectA` and `expectedKeys`
-// should return `true`
-const objectA = {
-  id: 2,
-  name: 'Jane Doe',
-  age: 34,
-  city: 'Chicago',
-};
-
-// running the function with `objectB` and `expectedKeys`
-// should return `false`
-const objectB = {
-  id: 3,
-  age: 33,
-  city: 'Peoria',
-};
-
-const expectedKeys = ['id', 'name', 'age', 'city'];
-
-function validateKeys(object, expectedKeys) {
-  const keys = Object.keys(object);
-    if(keys.length !== expectedKeys.length) {
-      return false;
-    }
-    for (let i = 0; i < expectedKeys.length; i++) {
-      if (!keys.find(key => key === expectedKeys[i])) {
-        return false;
-      }
-    }
-    return true;
-}
-
-/* From here down, you are not expected to 
-   understand.... for now :)  
-   
-   
-   Nothing to see here!
-   
-*/
-
-function testIt() {
-  const objectA = {
-    id: 2,
-    name: 'Jane Doe',
-    age: 34,
-    city: 'Chicago',
-  };
-
-  const objectB = {
-    id: 3,
-    age: 33,
-    city: 'Peoria',
-  };
-
-  const objectC = {
-    id: 9,
-    name: 'Billy Bear',
-    age: 62,
-    city: 'Milwaukee',
-    status: 'paused',
-  };
-
-  const objectD = {
-    foo: 2,
-    bar: 'Jane Doe',
-    bizz: 34,
-    bang: 'Chicago',
-  };
-
-  const expectedKeys = ['id', 'name', 'age', 'city'];
-
-  if (typeof validateKeys(objectA, expectedKeys) !== 'boolean') {
-    console.error('FAILURE: validateKeys should return a boolean value');
-    return;
-  }
-
-  if (!validateKeys(objectA, expectedKeys)) {
-    console.error(
-      `FAILURE: running validateKeys with the following object and keys
-      should return true but returned false:
-      Object: ${JSON.stringify(objectA)}
-      Expected keys: ${expectedKeys}`
-    );
-    return;
-  }
-
-  if (validateKeys(objectB, expectedKeys)) {
-    console.error(
-      `FAILURE: running validateKeys with the following object and keys
-      should return false but returned true:
-      Object: ${JSON.stringify(objectB)}
-      Expected keys: ${expectedKeys}`
-    );
-    return;
-  }
-
-  if (validateKeys(objectC, expectedKeys)) {
-    console.error(
-      `FAILURE: running validateKeys with the following object and keys
-      should return false but returned true:
-      Object: ${JSON.stringify(objectC)}
-      Expected keys: ${expectedKeys}`
-    );
-    return;
-  }
-
-  if (validateKeys(objectD, expectedKeys)) {
-    console.error(
-      `FAILURE: running validateKeys with the following object and keys
-      should return false but returned true:
-      Object: ${JSON.stringify(objectD)}
-      Expected keys: ${expectedKeys}`
-    );
-    return;
-  }
-
-  console.log('SUCCESS: validateKeys is working');
-}
-
-testIt();
+console.log(findByID(dataID, 1))
--- objectDrills3.js
+++ objectDrills3.js
@@ -1,29 +1,12 @@
-'use strict';
+"use strict";
 
 const loaf = {
-  flour: 300,
-  water: 210,
-  hydration: function() {
-    return (this.water / this.flour) * 100
-  }
+	flour: 300,
+	water: 210,
+	hydration: function() {
+		return (this.water / this.flour) * 100;
+	}
 };
 
 console.log(loaf.flour, loaf.water);
 console.log(loaf.hydration());
-
-
-// Create an object with five properties: foo, bar, fum, quux, and spam. Give each property a unique value of your choosing.
-// Loop over the object using for ... in
-// Use console.log to show each property name and its associated value.
-
-const fuBar = {
-  foo: 'bar',
-  bar: 'biz',
-  fum: 'lum',
-  quux: 'luther',
-  spam: 'delicious'
-}
-
-for (const keys in fuBar) {
-  console.log(`{${keys}: ${fuBar[keys]}}`);
-}
