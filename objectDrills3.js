3a8280b29dfb29f4f2a59f45dbb468f85d46aab9
Jordan Heffernan <jheff2@gmail.com>
2018-12-05 15:10:37 -0800

    added constructor and fighters

--- objectDrills3.js
+++ objectDrills3.js
@@ -84,88 +84,92 @@
 
 // jobAndName;
 
-const letterCipher = {
-  a: 1,
-  b: 2,
-  c: 3,
-  d: 4
-}
+// const letterCipher = {
+//   a: 1,
+//   b: 2,
+//   c: 3,
+//   d: 4
+// }
 
-function decode (str, obj) {
-  let letter = ""
- switch(str[0]) {
+// function decode (str, obj) {
+//   let letter = ""
+//  switch(str[0]) {
 
-  case 'a':
-  letter = str.charAt(obj.a);
-  break;
+//   case 'a':
+//   letter = str.charAt(obj.a);
+//   break;
 
-  case 'b':
-  letter = str.charAt(obj.b);
-  break;
+//   case 'b':
+//   letter = str.charAt(obj.b);
+//   break;
 
-  case 'c':
-  letter = str.charAt(obj.c);
-  break;
+//   case 'c':
+//   letter = str.charAt(obj.c);
+//   break;
   
-  case 'd':
-  letter = str.charAt(obj.d);
-  break;
+//   case 'd':
+//   letter = str.charAt(obj.d);
+//   break;
 
-  default:
-  letter = " "
-  break;
-  }
- return letter;
-}
+//   default:
+//   letter = " "
+//   break;
+//   }
+//  return letter;
+// }
 
-function decodeWords (string) {
-	const arr = string.split(" ");
-	const answer = [];
-	for (i=0; i<arr.length; i++){
-		answer.push(decode(arr[i], letterCipher));
-	}
+// function decodeWords (string) {
+// 	const arr = string.split(" ");
+// 	const answer = [];
+// 	for (i=0; i<arr.length; i++){
+// 		answer.push(decode(arr[i], letterCipher));
+// 	}
 
-	console.log(answer.join(''));
-}
+// 	console.log(answer.join(''));
+// }
 
-decodeWords("craft block argon meter bells brown croon droop");
+// decodeWords("craft block argon meter bells brown croon droop");
 
-const test1 = new createCharacter('evan', 'billy', 'white', 'heaven', 10, 8 )
-const test2 = new createCharacter('Jordan', 'Big Cat', 'white', 'heaven', 7, 20)
+const test1 = new createCharacter("evan", "billy", "white", "heaven", 10, 8 );
+const test2 = new createCharacter("Jordan", "Big Cat", "white", "heaven", 7, 20);
 
-function createCharacter (name, nickname, race, origin, attack, defense ) {
-  return {
-    name,
-    nickname,
-    race,
-    origin,
-    attack,
-    defense,
-describe: function() {return `${name} is a ${race} from ${origin}.`},
-evaluateFight: function(character) {
-  let damage = character.attack - this.defense;
-  if (damage > 0) { 
-  return `Your opponent takes ${attack} damage and you recieve ${damage} damage.`
-} else {return `Your opponent takes ${attack} damage and you recieve 0 damage.`}
-}
-}
+function createCharacter (name, nickname, race, origin, attack, defense, ) {
+	return {
+		name,
+		nickname,
+		race,
+		origin,
+		attack,
+		defense,
+		describe: function() {return `${name} is a ${race} from ${origin}.`;},
+		evaluateFight: function(character) {
+			let damage = character.attack - this.defense;
+			if (damage > 0) { 
+				return `Your opponent takes ${attack} damage and you recieve ${damage} damage.`;
+			} else {return `Your opponent takes ${attack} damage and you recieve 0 damage.`;}
+		}
+	};
 }
 
 const characters = [
-createCharacter("Gandalf the White", "gandalf", "Wizard", "Middle Earth", 10, 6,), 
-createCharacter("Bilbo Baggins", "bilbo", "Hobbit", "The Shire", 2, 1), 
-createCharacter("Frodo Baggins", "frodo", "Hobbit", "The Shire", 3, 2),
-createCharacter("Aragorn son of Arathorn", "aragorn", "Man", "Dunnedain", 6, 8),
-createCharacter("Legolas", "legolas", "Elf", "Woodland Realm", 8, 5),
-createCharacter("Arwen Undomiel", "Arwen", "Half-Elf", "Rivendell", 40, 80)
-
+	createCharacter("Gandalf the White", "gandalf", "Wizard", "Middle Earth", 10, 6,), 
+	createCharacter("Bilbo Baggins", "bilbo", "Hobbit", "The Shire", 2, 1), 
+	createCharacter("Frodo Baggins", "frodo", "Hobbit", "The Shire", 3, 2),
+	createCharacter("Aragorn son of Arathorn", "aragorn", "Man", "Dunnedain", 6, 8),
+	createCharacter("Legolas", "legolas", "Elf", "Woodland Realm", 8, 5),
+	createCharacter("Arwen Undomiel", "Arwen", "Half-Elf", "Rivendell", 40, 80)
 ];
 
-const aragorn = characters.find(character => character.nickname === 'aragorn')
-console.log(aragorn.describe())
 
-const hobbits = characters.filter(character => character.race === 'Hobbit');
 
-console.log(hobbits);
 // console.log(test1.describe());
 // console.log(test2.evaluateFight(test1))
+
+// const aragorn = characters.find(character => character.nickname === 'aragorn')
+// console.log(aragorn.describe())
+
+// const hobbits = characters.filter(character => character.race === 'Hobbit');
+// console.log(hobbits);
+
+// const fighters = characters.filter(character => character.attack > 5);
+// console.log(fighters);
