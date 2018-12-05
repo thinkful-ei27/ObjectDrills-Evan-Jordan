27b395e58112fdfa77317fae2880b3906a2f6dcd
Evan Guirino <etg624@gmail.com>
2018-12-05 09:05:18 -0800

    added delete function

--- objectCreator.js
+++ objectCreator.js
@@ -37,3 +37,19 @@
     return person;
   }
   
+
+  const sampleObj = {
+    foo: 'foo',
+    bar: 'bar',
+    bizz: 'bizz',
+    bang: 'bang',
+  };
+  
+  function keyDeleter(obj) {
+    delete obj.foo;
+    delete obj.bar;
+
+    return obj;
+  }
+  
+  
