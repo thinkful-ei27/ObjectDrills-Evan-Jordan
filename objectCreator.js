0e9485c3ac84dec1f68397ea02c83de32723cb93
Jordan Heffernan <jheff2@gmail.com>
2018-12-05 08:40:51 -0800

    added objectCreator

---
+++ objectCreator.js
@@ -0,0 +1,9 @@
+function createMyObject() {
+    const myObject = {
+        foo: 'bar',
+        answerToUniverse: 42,
+        'olly olly': 'oxen free',
+        sayHello: function() {return "hello"}
+    }
+    return myObject;
+  }
