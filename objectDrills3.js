ad96f651659521c9435cd9bdd7199bff207fc01e
Evan Guirino <etg624@gmail.com>
2018-12-05 11:40:41 -0800

    for in practice

--- objectDrills3.js
+++ objectDrills3.js
@@ -10,3 +10,20 @@ const loaf = {
 
 console.log(loaf.flour, loaf.water);
 console.log(loaf.hydration());
+
+
+// Create an object with five properties: foo, bar, fum, quux, and spam. Give each property a unique value of your choosing.
+// Loop over the object using for ... in
+// Use console.log to show each property name and its associated value.
+
+const fuBar = {
+  foo: 'bar',
+  bar: 'biz',
+  fum: 'lum',
+  quux: 'luther',
+  spam: 'delicious'
+}
+
+for (const keys in fuBar) {
+  console.log(`{${keys}: ${fuBar[keys]}}`);
+}
