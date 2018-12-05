ba1fc061b3c589ff811b9057ae9a932792a50ae4
Evan Guirino <etg624@gmail.com>
2018-12-05 08:57:00 -0800

    added updateObject function

--- objectCreator.js
+++ objectCreator.js
@@ -1,9 +1,65 @@
-function createMyObject() {
-    const myObject = {
-        foo: 'bar',
-        answerToUniverse: 42,
-        'olly olly': 'oxen free',
-        sayHello: function() {return "hello"}
-    }
-    return myObject;
-  }
+// function createMyObject() {
+//     const myObject = {
+//         foo: 'bar',
+//         answerToUniverse: 42,
+//         'olly olly': 'oxen free',
+//         sayHello: function() {return "hello"}
+//     }
+//     return myObject;
+//   }
+
+
+const cat = {
+    name: 'meowzers',
+    age: '3'
+}
+
+  function updateObject(obj) {
+   
+    obj.foo = 'foo'
+    obj.bar = 'bar'
+    obj.bizz = 'bizz'
+    obj.bang = 'bang'
+    
+    return obj;
+}
+  
+  updateObject(cat)
+  
+  /* From here down, you are not expected to 
+     understand.... for now :)  
+     
+     
+     Nothing to see here!
+     
+  */
+  
+//   (function testUpdateObject() {
+//     var oldObj = {
+//       cats: 'cats',
+//       dogs: 'dogs',
+//     };
+//     var newObj = updateObject(oldObj);
+//     if (typeof newObj !== 'object') { 
+//       console.error('ERROR: `createMyObject` must return an object');
+//       return false
+//     }
+//     ['foo', 'bar', 'bizz', 'bang'].forEach(function(key) {
+//       if (!(key in newObj)) {
+//         console.error('ERROR: `' + key + '` not in object returned by `updateObject`');
+//         return false;
+//       }
+//     });
+//     ['foo', 'bar', 'bizz', 'bang'].forEach(function(key) {
+//       if (newObj[key] !== key) {
+//         console.error('ERROR: `' + key + '` should be "' + key + '" but was ' + newObj[key]);
+//         return false;
+//       }
+//     });
+//     if (!(newObj.cats === 'cats' && newObj.dogs === 'dogs')) {
+//       console.error('ERROR: your function doesn\'t preserve existing key/value pairs');
+//       return false;
+//     }
+//     console.log('SUCCESS: `updateObject` works correctly!');
+    
+//   })();
